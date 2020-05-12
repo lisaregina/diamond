17468946ac9f5918d712445ce7daabe9edd1be1e
lisaregina <64302527+lisaregina@users.noreply.github.com>
2020-05-12 15:56:41 +0200

    Add files via upload
    hier ein Programm, dass das Alphabet bis zu einem bestimmten Buchstaben ausgibt, mit Test, der nicht funktioniert,weil irgendetwas mit den Klammern nicht funktioniert

---
+++ diamond.js
@@ -0,0 +1,10 @@
+
+function diamond(alpha, end){        //alpha -> array für alle buchstaben im Alphabet
+    const end;
+    
+    // alpha.indexOf(end);
+    
+    // const letter = alpha.map(
+    // alpha.slice(0, end +1)); //end+1, weil sonst der gegebene Buchstabe sonst nicht enthalten ist
+
+}
