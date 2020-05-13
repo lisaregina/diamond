25ec68f9eb79bf894563e950efa895d0e83c7c20
lisaregina <64302527+lisaregina@users.noreply.github.com>
2020-05-13 10:51:45 +0200

    Add files via upload
    einfache Tests hinzugefügt, wie ob test funktioniert und ob, der Buchstabe im Array enthalten ist

--- diamond.test.js
+++ diamond.test.js
@@ -1,7 +1,21 @@
 const {diamond} = require("./diamond");
-describe("diamond", () =>{
-    it("should have letter, given from end, in alpha", ()=>{
-        console.log(end = D);
-        expect(diamond(alpha, D)).toEqual(D)
-    )}
-)};
+
+describe("diamond", ()=>{
+    it("shoul pass the test", ()=>{
+        console.log("D");
+        expect("D").toEqual("D")
+    })
+
+    // it("should be the same letter", ()=>{
+    //     //console.log(diamond("A"));
+    //     expect(diamond("A")).toEqual("A")
+    // })
+
+    it("should be the within array", ()=>{
+        console.log(diamond("B"));
+        expect(diamond("B")).toEqual("B")
+    })
+});
+
+
+    
