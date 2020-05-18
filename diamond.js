04032b425e42d31a2103266aca3da063263ed0a4
lisaregina <64302527+lisaregina@users.noreply.github.com>
2020-05-18 20:58:23 +0200

    Add files via upload

--- diamond.js
+++ diamond.js
@@ -3,44 +3,35 @@ function diamond(letter){
     
    const alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-  
       end = alpha.indexOf(letter);
+
       let bet = [];
       
-      bet = alpha.map(          //erstellt ein Array bet, dass alpha beinhaltet mit den geslicten Elementen
-                      letter => alpha.slice(0, end + 1)
-                     );      //end+1, weil sonst der gegebene Buchstabe nicht enthalten ist
+      bet = alpha.slice(0, end + 1);//erstellt ein Array bet, dass alpha beinhaltet mit den geslicten Elementen
+                                              //end+1, weil sonst der gegebene Buchstabe nicht enthalten ist
       
+    
   let symbol = '-';
 
-  let demo = [];          //Array, zum Ausgeben des Diamanten
-  
+  let dia = [];          //Array, zum Ausgeben des Diamanten
+
 
   Array.prototype.forEach.call(bet, elem =>{
-    for(let i= bet.indexOf[elem]; i<=end+1; i++){
-      demo.push(symbol);
-    }
-    demo.push(elem);
-    //hier die Symbole in der Mitte
-    for(let j =bet.indexOf[elem] > 1; j === end +1; j++){
-      demo.push(symbol);
-    }
-    demo.push(elem);
-    for(let i= bet.indexOf[elem]; i<=end+1; i++){
-      demo.push(symbol);
-    }
-    demo.push('<br>');
-  })    
+     let currIndex = bet.indexOf(elem);
+      for(i=currIndex; i>=end; i++){
+         dia.push(symbol)
+      }
+  });
 
 
-      console.log(letter);
-      console.log(bet);
-      console.log(demo);
-    
-  
+  console.log(letter);
+  console.log(bet);
+  console.log(dia);
+
 
 return letter;
 return bet;
-return demo;
+return dia;
+
 };
 module.exports = {diamond};
--- diamond.test.js
+++ diamond.test.js
@@ -2,35 +2,23 @@ const {diamond} = require("./diamond");
 
 describe("diamond", ()=>{
 
-//     it("should pass the test", ()=>{
-//         console.log("D");
-//         expect("D").toEqual("D")
-//     })
+    it("should pass the test", ()=>{
+        console.log("D");
+        expect("D").toEqual("D")
+    })
 
-    // it("should be the same letter", ()=>{
-    //     //console.log(diamond("A"));
-    //     expect(diamond("A")).toEqual("A")
-    // })
-
-    // it("should be within the array", ()=>{
-    //     console.log(diamond(letter = "B"));
-    //     expect(diamond(letter = "B")).toEqual("B")
-    // })
+    it("should be the same letter", ()=>{
+        //console.log(diamond("A"));
+        expect(diamond('D')).toEqual(letter = "D")
+    })
 
     it("should display a new array, containing the beginning of the alphabet till the letter", ()=>{
-        console.log(diamond(letter = "D"));
-        expect(diamond(bet = ["A", "B", "C", "D"])).toEqual(["A", "B", "C", "D"])
+        expect(diamond("F")).toEqual(letter = "F", bet =[ 'A', 'B', 'C', 'D', 'E', 'F'])
     })
 
-    it("should display the index-amount of symbols", ()=>{
-        console.log(diamond(letter = "B"));
-        expect(diamond(demo=['-', '-'])).toEqual(demo=['-', '-'])
-    })
-
-    it("should display symbols and letter and symbols", ()=>{
-        console.log(diamond(letter = "C"));
-        expect(diamond(demo=[ '-', '-', 'A','-', '-', '-', 'B', '-', 'B', '-', 'C', '-', '-', '-', 'C'])).toEqual(demo=[ '-', '-', 'A','-', '-', '-', 'B', '-', 'B', '-', 'C', '-', '-', '-', 'C'])
-    })
+    // it("should display symbol with element", ()=>{
+    //     expect(diamond("D")).toEqual(dia=['-', 'A', '-', 'B','-', 'C', '-', 'D'])
+    // })
 
 });
 
